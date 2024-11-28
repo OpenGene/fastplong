e1846e7c7ba4f25ea0425d904130d86b34830839
benney_au@hotmail.com <benney@DESKTOP-DQNT22A.>
2024-11-29 00:46:16 +1100

    move fastareader_test.cpp

---
+++ test/fastareader_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "fastareader.h"
+
+TEST(fastareader, test) {
+    /*FastaReader reader("testdata/tinyref.fa");
+      reader.readAll();
+
+      string contig1 = "GATCACAGGTCTATCACCCTATTAATTGGTATTTTCGTCTGGGGGGTGTGGAGCCGGAGCACCCTATGTCGCAGT";
+      string contig2 = "GTCTGCACAGCCGCTTTCCACACAGAACCCCCCCCTCCCCCCGCTTCTGGCAAACCCCAAAAACAAAGAACCCTA";
+
+      if(reader.mAllContigs.count("contig1") == 0 || reader.mAllContigs.count("contig2") == 0 )
+      return false;
+
+      if(reader.mAllContigs["contig1"] != contig1 || reader.mAllContigs["contig2"] != contig2 )
+      return false;*/
+
+}
