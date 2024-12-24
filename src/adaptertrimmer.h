0c9e505084074975940f28f48cc1144ab8a283ef
benney <benney@DESKTOP-DQNT22A.>
2024-12-24 16:59:52 +1100

    add tests for adaptertrimmer

--- src/adaptertrimmer.h
+++ src/adaptertrimmer.h
@@ -19,7 +19,7 @@ public:
     static int trimBySequenceEnd(Read* r1, FilterResult* fr, string& adapter, double edMax = 0.3, int trimmingExtension = 10);
     static int trimByMultiSequences(Read* r1, FilterResult* fr, vector<string>& adapterList, double edMax = 0.3, int trimmingExtension = 10);
     static bool findMiddleAdapters(Read* r, string& startAdater, string& endAdapter, int& start, int& len, double edMax = 0.3, int trimmingExtension = 10);
-    static int searchAdapter(string* read, string& adapter, double edMax = 0.3, int searchStart = 0, int searchLen = -1, bool asLeftAsPossible = false, bool asRightAsPossible = false);
+    static int searchAdapter(string* read, const string& adapter, double edMax = 0.3, int searchStart = 0, int searchLen = -1, bool asLeftAsPossible = false, bool asRightAsPossible = false);
 };
 
 
--- test/adaptertrimmer_test.cpp
+++ test/adaptertrimmer_test.cpp
@@ -17,18 +17,41 @@ TEST(AdapterTrimmer, trimBySequenceStart) {
     trimmed = AdapterTrimmer::trimBySequenceEnd(&r2, NULL, adapter, 0.3, 0);
     EXPECT_EQ(*r2.mSeq, "TTTTAACCCCCCCCCCCCCCCCCCCCCCCCCCCCAATTTTAAAA");
 
-    /*Read read("@name",
+    // Read read("@name",
+    //     "TTTTAACCCCCCCCCCCCCCCCCCCCCCCCCCCCAATTTTAAAATTTTCCCCGGGGAAATTTCCCGGGAAATTTCCCGGGATCGATCGATCGATCGAATTCC",
+    //     "+",
+    //     "///EEEEEEEEEEEEEEEEEEEEEEEEEE////EEEEEEEEEEEEE////E////EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    // vector<string> adapterList;
+    // adapterList.push_back("GCTAGCTAGCTAGCTA");
+    // adapterList.push_back("AAATTTCCCGGGAAATTTCCCGGG");
+    // adapterList.push_back("ATCGATCGATCGATCG");
+    // adapterList.push_back("AATTCCGGAATTCCGG");
+    // trimmed = AdapterTrimmer::trimByMultiSequences(&read, NULL, adapterList);
+    // if (*read.mSeq != "TTTTAACCCCCCCCCCCCCCCCCCCCCCCCCCCCAATTTTAAAATTTTCCCCGGGG") {
+    //     cerr << read.mSeq << endl;
+    //     return false;
+    // }
+}
+
+
+TEST(AdapterTrimmer, searchAdapterLeft) {
+    Read read("@name",
         "TTTTAACCCCCCCCCCCCCCCCCCCCCCCCCCCCAATTTTAAAATTTTCCCCGGGGAAATTTCCCGGGAAATTTCCCGGGATCGATCGATCGATCGAATTCC",
         "+",
         "///EEEEEEEEEEEEEEEEEEEEEEEEEE////EEEEEEEEEEEEE////E////EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
-    vector<string> adapterList;
-    adapterList.push_back("GCTAGCTAGCTAGCTA");
-    adapterList.push_back("AAATTTCCCGGGAAATTTCCCGGG");
-    adapterList.push_back("ATCGATCGATCGATCG");
-    adapterList.push_back("AATTCCGGAATTCCGG");
-    trimmed = AdapterTrimmer::trimByMultiSequences(&read, NULL, adapterList);
-    if (*read.mSeq != "TTTTAACCCCCCCCCCCCCCCCCCCCCCCCCCCCAATTTTAAAATTTTCCCCGGGG") {
-        cerr << read.mSeq << endl;
-        return false;
-    }*/
+    string adapter = "TTTT";
+    int pos = AdapterTrimmer::searchAdapter(
+        read.mSeq, adapter, 0.3, 0, -1, true, false);
+    EXPECT_EQ(pos, 0);
+}
+
+TEST(AdapterTrimmer, searchAdapterLeft2) {
+    Read read("@name",
+        "TTTTAACCCCCCCCCCCCCCCCCCCCCCCCCCCCAATTTTAAAATTTTCCCCGGGGAAATTTCCCGGGAAATTTCCCGGGATCGATCGATCGATCGAATTCC",
+        "+",
+        "///EEEEEEEEEEEEEEEEEEEEEEEEEE////EEEEEEEEEEEEE////E////EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    string adapter = "AACC";
+    int pos = AdapterTrimmer::searchAdapter(
+        read.mSeq, adapter, 0.3, 0, -1, true, false);
+    EXPECT_EQ(pos, 4);
 }
