639a7b1f4adf80bc1860f9283fb4877da33c9f1c
Shifu Chen <chen@haplox.com>
2025-04-17 10:11:29 +0800

    fix a bug in fastqreader
    handle the case that \r or \n in the start of buf

--- src/fastqreader.cpp
+++ src/fastqreader.cpp
@@ -252,6 +252,12 @@ void FastqReader::getLine(string* line){
 		readToBuf();
 		start = 0;
 		end = 0;
+		// handle the case that \r or \n in the start of buf
+		if(line->empty()) {
+			while(start < mBufDataLen && (mFastqBuf[start] == '\r' || mFastqBuf[start] == '\n'))
+				start++;
+			end = start;
+		}
 		while(end < mBufDataLen) {
 			if(mFastqBuf[end] != '\r' && mFastqBuf[end] != '\n')
 				end++;
@@ -318,6 +324,7 @@ Read* FastqReader::read(){
 	getLine(quality);
 
 	if (strand->empty() || (*strand)[0]!='+') {
+		cerr << *name << endl;
 		cerr << "Expected '+', got " << *strand << endl;
 		error_exit("'+' expected");
 	}
