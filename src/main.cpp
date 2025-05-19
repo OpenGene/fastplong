6305918f3894a4f8ec7038b8babb167cc692dc5b
Shifu Chen <chen@haplox.com>
2025-05-19 10:21:53 +0800

    output --help and --version to stdout, instead of stderr

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]){
         //return 0;
     }
     if (argc == 2 && (strcmp(argv[1], "-v")==0 || strcmp(argv[1], "--version")==0)){
-        cerr << "fastplong " << FASTPLONG_VER << endl;
+        cout << "fastplong " << FASTPLONG_VER << endl;
         return 0;
     }
     cmdline::parser cmd;
