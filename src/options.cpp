2281fe0eabc8f5abdb256c82663b026910081350
bart <43843876+bartns@users.noreply.github.com>
2025-12-19 16:40:45 +0000

    Fix warning message for thread limit in options.cpp

--- src/options.cpp
+++ src/options.cpp
@@ -120,7 +120,7 @@ bool Options::validate() {
     if(thread < 1) {
         thread = 1;
     } else if(thread > 16) {
-        cerr << "WARNING: fastp uses up to 16 threads although you specified " << thread << endl;
+        cerr << "WARNING: fastplong uses up to 16 threads although you specified " << thread << endl;
         thread = 16;
     }
 
